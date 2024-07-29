0a2bfa7c0cf586ef608cea64b3f1e125e3f74e29
Florian Tischler <Florian.Tischler@gmx.net>
2024-07-29 14:17:39 +0200

    enable dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {
   MatCard,
@@ -10,6 +10,7 @@ import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
 import { NavigationComponent } from './navigation/navigation.component';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -29,5 +30,14 @@ import { NavigationComponent } from './navigation/navigation.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular18-m3';
+  private renderer = inject(Renderer2);
+  private document = inject(DOCUMENT);
+
+  enableDarkTheme() {
+    this.renderer.addClass(this.document.documentElement, 'dark-theme');
+  }
+
+  disableDarkTheme() {
+    this.renderer.removeClass(this.document.documentElement, 'dark-theme');
+  }
 }

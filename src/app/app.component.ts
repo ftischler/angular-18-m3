0c673601c60e77569ab2c66787a092268e43e6a0
Florian Tischler <Florian.Tischler@gmx.net>
2024-08-21 11:25:20 +0200

    add material menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Renderer2 } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {
   MatCard,
@@ -10,7 +10,15 @@ import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
 import { NavigationComponent } from './navigation/navigation.component';
-import { DOCUMENT } from '@angular/common';
+import { KeyValuePipe, NgTemplateOutlet } from '@angular/common';
+import { MatChip } from '@angular/material/chips';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
+import { ValueOf } from 'type-fest';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -25,19 +33,46 @@ import { DOCUMENT } from '@angular/common';
     MatInput,
     MatButton,
     NavigationComponent,
+    MatChip,
+    MatMenu,
+    MatCheckbox,
+    MatMenuItem,
+    MatMenuTrigger,
+    ReactiveFormsModule,
+    KeyValuePipe,
+    NgTemplateOutlet,
+    MatIcon,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  private renderer = inject(Renderer2);
-  private document = inject(DOCUMENT);
+  private formBuilder = inject(FormBuilder);
 
-  enableDarkTheme() {
-    this.renderer.addClass(this.document.documentElement, 'dark-theme');
-  }
+  formGroup = this.formBuilder.nonNullable.group({
+    element1: false,
+    element2: false,
+    element3: false,
+    element4: this.formBuilder.group({
+      subElement1: false,
+      subElement2: false,
+    }),
+  });
 
-  disableDarkTheme() {
-    this.renderer.removeClass(this.document.documentElement, 'dark-theme');
+  amountSelected = toSignal(
+    this.formGroup.valueChanges.pipe(map(() => this.getSelectedAmount())),
+    { initialValue: this.getSelectedAmount() },
+  );
+
+  getSelectedAmount(
+    parent:
+      | typeof this.formGroup.value
+      | ValueOf<typeof this.formGroup.value> = this.formGroup.value,
+  ): number {
+    return Object.values(parent)
+      .map((value) =>
+        typeof value === 'object' ? this.getSelectedAmount(value) : value,
+      )
+      .filter(Boolean).length;
   }
 }
